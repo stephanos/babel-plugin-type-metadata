192eb2efcec51525dd7c6e0716e33892b1723d8f
stephanos <github@stephanbehnke.de>
2016-05-05 23:00:15 +0200

    add tests for nullable types

--- src/serializer.spec.js
+++ src/serializer.spec.js
@@ -18,14 +18,29 @@ describe('serialize', () => {
     assert.equal(descr, '{ type: Boolean }');
   });
 
+  it('a nullable boolean', () => {
+    const descr = serialize({ type: 'NullableTypeAnnotation', typeAnnotation: { type: 'BooleanTypeAnnotation' } });
+    assert.equal(descr, '{ type: Boolean, nullable: true }');
+  });
+
   it('a number', () => {
     const descr = serialize({ type: 'NumberTypeAnnotation' });
     assert.equal(descr, '{ type: Number }');
   });
 
+  it('a nullable number', () => {
+    const descr = serialize({ type: 'NullableTypeAnnotation', typeAnnotation: { type: 'NumberTypeAnnotation' } });
+    assert.equal(descr, '{ type: Number, nullable: true }');
+  });
+
   it('a string', () => {
     const descr = serialize({ type: 'StringTypeAnnotation' });
     assert.equal(descr, '{ type: String }');
+  });
+
+  it('a nullable string', () => {
+    const descr = serialize({ type: 'NullableTypeAnnotation', typeAnnotation: { type: 'StringTypeAnnotation' } });
+    assert.equal(descr, '{ type: String, nullable: true }');
   });
 
   it('a void', () => {
